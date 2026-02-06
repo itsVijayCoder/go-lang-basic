5eb3ae9c42ccfa5c1ba335ca9abe73550ca910a1
itsVijayCoder <vijaysubramanian1539@gmail.com>
2026-02-06 16:06:41 +0530

    add initial Go files and .gitignore for project setup

---
+++ 02_Variables_and_Types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Vijay"
+	age := 25
+	var isLearning bool = true
+	var height float32 = 5.9
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Learning Go:", isLearning)
+	fmt.Println("Height:", height)
+}
